ee9eb4915899c7d1fa0416f891f4a9571febc5bd
Saqib <hafizsaqi555@gmail.com>
2026-01-05 11:25:19 +0500

    fix: test commands

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -14,9 +14,9 @@ describe('AppController', () => {
     appController = app.get<AppController>(AppController);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  describe('checkHealth', () => {
+    it('should return { status: "OK" }', () => {
+      expect(appController.checkHealth()).toEqual({ status: 'OK' });
     });
   });
 });
